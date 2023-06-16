eb9f8a4e036227f028144f6d8ad4cdf5b999f3c7
hymnstar <dlcksrb908@naver.com>
2023-06-16 14:27:08 +0900

    Implement version#2

--- menu_selector.py
+++ menu_selector.py
@@ -1,4 +1,12 @@
+import file_manager
+import parking_spot_manager
+
 def start_process(path):
+    # 파일에서 문자열 리스트를 가져온다
+    str_list = file_manager.read_file(path)
+    # 문자열 리스트를 parking_spot 객체 리스트로 변환한다
+    spots = parking_spot_manager.str_list_to_class_list(str_list)
+
     while True:
         print("---menu---")
         print("[1] print")
@@ -6,10 +14,13 @@ def start_process(path):
         print("[3] sort")
         print("[4] exit")
         select = int(input('type:'))
+
         if select == 1:
-            print("not implemented yet")
-            # fill this block
+            # parking_spot 객체 리스트를 출력한다
+            parking_spot_manager.print_spots(spots)
+
         elif select == 2:
+            # 필터 기능 
             print("---filter by---")
             print("[1] name")
             print("[2] city")
@@ -19,40 +30,38 @@ def start_process(path):
             select = int(input('type:'))
             if select == 1:
                 keyword = input('type name:')
-                print("not implemented yet")
-                # fill this block
+                # 이름 추가
             elif select == 2:
                 keyword = input('type city:')
-                print("not implemented yet")
-                # fill this block
+                # 도시 추가
             elif select == 3:
                 keyword = input('type district:')
-                print("not implemented yet")
-                # fill this block
+                # 지역구 추가
             elif select == 4:
                 keyword = input('type ptype:')
-                print("not implemented yet")
-                # fill this block
+                # 타입 추가
             elif select == 5:
                 min_lat = float(input('type min lat:'))
                 max_lat = float(input('type max lat:'))
                 min_lon = float(input('type min long:'))
                 max_lon = float(input('type max long:'))
-                print("not implemented yet")
-                # fill this block
+                # 위치 추가
             else:
                 print("invalid input")
+
         elif select == 3:
+            # 정렬 기능 
             keywords = ['name', 'city', 'district', 'ptype', 'latitude', 'longitude']
             print("---sort by---")
             print(keywords)
             keyword = input('type keyword:')
-            if keyword in keywords:
-                print("not implemented yet")
-                # fill this block
-            else: print("invalid input")
+            # 정렬 관련 코드 추가
+
         elif select == 4:
-            print("not implemented yet")
-            # fill this block
+            # 프로그램 종료
+            print("Exit")
+            break
+
         else:
             print("invalid input")
+
--- parking_spot_manager.py
+++ parking_spot_manager.py
@@ -1,26 +1,50 @@
 class parking_spot:
-    # you have to implement 'constructor(생성자)' and 'get' method
+    def __init__(self, name, city, district, ptype, longitude, latitude):
+        # 주차장 정보를 딕셔너리로 저장한다
+        self.__item = {'name': name, 'city': city, 'district': district, 'ptype': ptype, 'longitude': float(longitude), 'latitude': float(latitude)}
+
+    def get(self, keyword='name'):
+        # 특정 정보를 가져온다
+        return self.__item.get(keyword)
+
     def __str__(self):
+        # 주차장 정보를 깔끔한 문자열로 표시 
         item = self.__item
-        s  = f"[{item['name']}({item['ptype']})] "
-        s += f"{item['city']} {item['district']}"
+        s = f"[{item['name']}({item['ptype']})] "
+        s += f"{item['city']} {item['district']} "
         s += f"(lat:{item['latitude']}, long:{item['longitude']})"
         return s
+
+
+def str_list_to_class_list(str_list):
+    # 문자열 리스트를 객체 리스트로 변환한다
+    class_list = []
+    for s in str_list:
+        items = s.split(',')
+        class_list.append(parking_spot(items[1], items[2], items[3], items[4], items[5], items[6]))
+    return class_list
+
+
+def print_spots(spots):
+    # 객체 리스트를 출력
+    print(f"---print elements({len(spots)})---")
+    for spot in spots:
+        print(spot)
 
 
 # 각 단계별로 테스트 (테스트할때 주석해제 후 사용)
 if __name__ == '__main__':
     print("Testing the module...")
-    # version#2
-    # import file_manager
-    # str_list = file_manager.from_file("./input/free_parking_spot_seoul.csv")
-    # spots = str_list_to_class_list(str_list)
-    # print_spots(spots)
+    #version#2
+    import file_manager
+    str_list = file_manager.from_file("./input/free_parking_spot_seoul.csv")
+    spots = str_list_to_class_list(str_list)
+    print_spots(spots)
 
     # version#3
     # spots = filter_by_district(spots, '동작')
     # print_spots(spots)
-    
+
     # version#4
     # spots = sort_by_keyword(spots, 'name')
     # print_spots(spots)
